638b7522bd905aa2abf2086edecd3281abdfdad0
LeoDuffed <carlosmroder@gmail.com>
2024-10-30 15:21:13 -0600

    Haciendo la version final de la App
    En construccion

--- APP_lista_super.py
+++ APP_lista_super.py
@@ -1,6 +1,4 @@
 # Este es un ejemplo de como se usa kivy.
-
-
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label 
---
+++ APP_lista_super2.0.py
@@ -0,0 +1,8 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button 
+
+class ListaSuper(App):
+    def build (self):
