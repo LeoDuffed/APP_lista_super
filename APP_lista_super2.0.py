09786a8dceb61afef8e36ffa37a432a5eec12288
LeoDuffed <carlosmroder@gmail.com>
2024-11-05 14:06:32 -0600

    pequeños cambios

--- APP_lista_super2.0.py
+++ APP_lista_super2.0.py
@@ -12,16 +12,16 @@ class ListaSuperApp(App):
 
         layout = BoxLayout(orientation='vertical', padding=20, spacing=10)
 
-        instruction_label = Label(text="Ingrese el nombre del producto y su costo", font_size='30sp', color = (0, 0, 0, 1))
+        instruction_label = Label(text="App lista del super", font_size='30sp', color = (0, 0, 0, 1))
         layout.add_widget(instruction_label)
 
-        self.producto_input = TextInput(hint_text="Ingrese nombre del producto", font_size='16sp', multiline=False, size_hint_y=None, height=60)
+        self.producto_input = TextInput(hint_text="Ingrese nombre del producto", font_size='16sp', multiline=False, size_hint_y=None, height=100)
         layout.add_widget(self.producto_input)
 
-        self.costo_input = TextInput(hint_text="Ingrese costo del producto", font_size='16sp', multiline=False, input_filter='float', size_hint_y=None, height=60)
+        self.costo_input = TextInput(hint_text="Ingrese costo del producto", font_size='16sp', multiline=False, input_filter='float', size_hint_y=None, height=100)
         layout.add_widget(self.costo_input)
 
-        agregar_button = Button(text="Agregar Producto", size_hint = (0.5,None),height = 100, pos_hint = {"center_x":0.5}, background_color=(0, 1, 0, 1))
+        agregar_button = Button(text="Agregar Producto", size_hint = (0.8,None),height = 200, pos_hint = {"center_x":0.5}, background_color=(0, 1, 0, 1))
         agregar_button.bind(on_press=self.agregar_producto)
         layout.add_widget(agregar_button)
 
@@ -29,7 +29,7 @@ class ListaSuperApp(App):
         layout.add_widget(self.suma_label)
 
         scroll_view = ScrollView(size_hint=(1, None), height=200)
-        self.lista_productos = BoxLayout(orientation='vertical', size_hint_y=None)
+        self.lista_productos = BoxLayout(orientation='vertical', size_hint_y=None, spacing = 20)
         self.lista_productos.bind(minimum_height=self.lista_productos.setter('height'))
         scroll_view.add_widget(self.lista_productos)
         layout.add_widget(scroll_view)
