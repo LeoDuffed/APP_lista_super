e71be60d90088c825e8732e91f44922b4d0b3b58
LeoDuffed <carlosmroder@gmail.com>
2024-11-07 00:13:16 -0600

    Ya quedo :))

--- App_lista_super3.0.py
+++ App_lista_super3.0.py
@@ -16,15 +16,15 @@ class PantallaInicio (Screen):
         WelcomeLabel = Label (text = "App Lista del Super", font_size = '30sp', color = (0,0,0,1) )
         layout.add_widget(WelcomeLabel)
 
-        boton_registro = Button(text = "Anotar productos", pos_hint = {"center_x": 0.5}, background_color = (0.6, 1, 0.6, 1))
+        boton_registro = Button(text = "Anotar productos", pos_hint = {"center_x": 0.5}, background_color = (0.5, 1, 0, 1))
         boton_registro.bind (on_press = self.CambiarRegistro) 
         layout.add_widget(boton_registro)
 
-        boton_verduras = Button(text = "Verduras y Fruta", pos_hint = {"center_x": 0.5}, background_color = (0.6, 1, 0.6, 1))
+        boton_verduras = Button(text = "Verduras y Frutas", pos_hint = {"center_x": 0.5}, background_color = (0.5, 1, 0, 1))
         boton_verduras.bind (on_press = self.CambiarVerduras) 
         layout.add_widget(boton_verduras)
 
-        boton_total = Button(text = "Total", pos_hint = {"center_x": 0.5}, background_color = (0.6, 1, 0.6, 1))
+        boton_total = Button(text = "Total", pos_hint = {"center_x": 0.5}, background_color = (0.5, 1, 0, 1))
         boton_total.bind (on_press = self.CambiarTotal) 
         layout.add_widget(boton_total)
 
@@ -43,27 +43,27 @@ class RegistroGastos (Screen):
     def __init__(self, **kwargs): 
         super().__init__(**kwargs)
 
-        Window.clearcolor = (1,1,1,1)
+        Window.clearcolor = (1, 1, 0.9, 1)
         
         self.layout = BoxLayout(orientation = 'vertical', padding = 20, spacing = 10)
 
-        self.producto_input = TextInput (hint_text = "Ingrese el articulo", multiline = False, size_hint_y = None, height = 80)
+        self.producto_input = TextInput (hint_text = "Ingrese el articulo", multiline = False, size_hint_y = None, height = 100, font_size= '16sp')
         self.layout.add_widget(self.producto_input)
 
-        self.precio_input = TextInput (hint_text = "Ingrese el precio del articulo", multiline = False, size_hint_y = None, height = 80, input_filter = 'float')
+        self.precio_input = TextInput (hint_text = "Ingrese el precio del articulo", multiline = False, size_hint_y = None, height = 100, input_filter = 'float', font_size = '16sp')
         self.layout.add_widget(self.precio_input)
 
-        self.cantidad_input = TextInput (hint_text = "Ingresa la cantidad de este producto", multiline = False, size_hint_y = None, height = 80, input_filter = 'int')
+        self.cantidad_input = TextInput (hint_text = "Ingresa la cantidad de este producto", multiline = False, size_hint_y = None, height = 100, input_filter = 'int', font_size = '16sp')
         self.layout.add_widget(self.cantidad_input)
 
-        boton_agregar = Button (text = "Agregar producto", size_hint = (0.5, None), height = 100, pos_hint = {"center_x":0.5}, background_color = (0,1,0,1))
+        boton_agregar = Button (text = "Agregar producto", size_hint = (0.8, None), height = 200, pos_hint = {"center_x":0.5}, background_color = (0,1,0,1))
         boton_agregar.bind(on_press = self.AgregarProducto)
         self.layout.add_widget(boton_agregar)
 
         self.resultado_label = Label(text = "", color = (0,0,0,1))
         self.layout.add_widget(self.resultado_label)
 
-        boton_volver = Button(text = "Volver", pos_hint = {"center_x": 0.5}, background_color = (0.6, 1, 0.6, 1))
+        boton_volver = Button(text = "Volver", pos_hint = {"center_x": 0.5}, background_color = (1, 0.7, 0.8, 1))
         boton_volver.bind (on_press = self.volver_registro) 
         self.layout.add_widget(boton_volver)
 
@@ -98,23 +98,23 @@ class VerdurasFrutas (Screen):
         insctruction_label = Label (text = "Verduras y Frutas", font_size = '30sp', color = (0,0,0,1))
         self.layout.add_widget(insctruction_label)
 
-        self.producto_input = TextInput (hint_text = "Ingresa el producto", multiline = False, font_size = '16sp', size_hint_y = None, height = 80) 
+        self.producto_input = TextInput (hint_text = "Ingresa el producto", multiline = False, font_size = '16sp', size_hint_y = None, height = 100) 
         self.layout.add_widget(self.producto_input)
 
-        self.precio_por_kilo = TextInput (hint_text = "Ingresa el valor por kilo", multiline = False, font_size = '16sp', size_hint_y = None, height = 80)
+        self.precio_por_kilo = TextInput (hint_text = "Ingresa el valor por kilo", multiline = False, font_size = '16sp', size_hint_y = None, height = 100)
         self.layout.add_widget(self.precio_por_kilo)
 
-        self.gramaje_input = TextInput (hint_text = "Ingresa en gramos lo que hagarraste", multiline = False, font_size = '16sp', size_hint_y = None, height = 80)
+        self.gramaje_input = TextInput (hint_text = "Ingresa en gramos lo que hagarraste", multiline = False, font_size = '16sp', size_hint_y = None, height = 100)
         self.layout.add_widget(self.gramaje_input)
 
-        agregar_button = Button (text = "Agregar producto", size_hint = (0.5, None), height = 100, pos_hint = {"center_x":0.5}, background_color = (0,1,0,1))
+        agregar_button = Button (text = "Agregar producto", size_hint = (0.8, None), height = 200, pos_hint = {"center_x":0.5}, background_color = (0,1,0,1))
         agregar_button.bind(on_press = self.agregar_producto)
         self.layout.add_widget(agregar_button)
 
         self.resultado_label = Label (text = "", color = (0,0,0,1))
         self.layout.add_widget(self.resultado_label)
         
-        boton_volver = Button (text = "Volver", pos_hint = {"center_x": 0.5}, background_color = (0,1,0,1))
+        boton_volver = Button (text = "Volver", pos_hint = {"center_x": 0.5}, background_color = (1, 0.7, 0.8, 1))
         boton_volver.bind (on_press= self.volver_registro)
         self.layout.add_widget(boton_volver)
 
@@ -152,14 +152,14 @@ class ListaTotal(Screen):
         self.scroll_view.add_widget(self.productos_layout)
         layout.add_widget(self.scroll_view)
 
-        boton_calcular_total = Button(text = "Calcular Total")
+        boton_calcular_total = Button(text = "Calcular Total", pos_hint = {"center_x": 0.5}, background_color = (0.6, 0.8, 1, 1))
         boton_calcular_total.bind(on_press = self.calcular_total)
         layout.add_widget(boton_calcular_total)
 
         self.total_label = Label(text = "")
         layout.add_widget(self.total_label)
 
-        boton_volver = Button(text = "Volver", pos_hint = {"center_x": 0.5}, background_color = (0,1,0,1))
+        boton_volver = Button(text = "Volver", pos_hint = {"center_x": 0.5}, background_color = (1, 0.7, 0.8, 1))
         boton_volver.bind (on_press = self.volver_registro) 
         layout.add_widget(boton_volver)
 
