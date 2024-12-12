077e3c9aa495be6a2323f6cb27ef2791ff28e162
LeoDuffed <carlosmroder@gmail.com>
2024-12-12 15:31:18 -0600

    Update App_lista_super5.0.py

--- App_lista_super5.0.py
+++ App_lista_super5.0.py
@@ -377,7 +377,7 @@ class ListaTotal(Screen):
         boton_comfirmar.bind(on_press = self.guardar_lista)
         content.add_widget(boton_comfirmar)
         content.add_widget(close_button)
-        popup = Popup(content = content, size_hint = (0.9,0.4), title = "Deseas Guardar tu lista")
+        popup = Popup(content = content, size_hint = (0.8,0.38), title = "Deseas Guardar tu lista")
         popup.open()
 
     def guardar_lista(self, instace):
